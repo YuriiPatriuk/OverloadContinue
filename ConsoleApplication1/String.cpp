45cf610b0f6c456f25b20771b577fd2a82e1ec01
Patr_sd37@student.itstep.org <Patr_sd37@student.itstep.org>
2020-11-27 20:57:02 +0200

    start

---
+++ ConsoleApplication1/String.cpp
@@ -0,0 +1,6 @@
+#include "String.h"
+#include "String.h"
+
+String::String()
+{
+}
---
+++ ConsoleApplication1/String.h
@@ -0,0 +1,11 @@
+#pragma once
+class String
+{
+public:
+	String();
+
+private:
+	char* str = nullptr;
+	size_t size;
+};
+
